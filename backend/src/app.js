ca2e9c3e943dc013077e8fcff2a1e3138a849e7c
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-02-20 17:33:23 +0530

    imported Express, CORS, Mongoose, and Socket.io, created an Express app, defined a /home route that returns {hello:world}, and started the server on port 8000 using an async function

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,13 +8,13 @@ import mongoose from "mongoose";
 
  const app = express();
 
- app.get("/home",(req, res)=>{
+ app.get("/home",(req, res)=>{  // home is the route
     return res.json({"hello":"world"});
  });
 
  const start = async () => {
 
-    app.listen(8000, () =>{
+    app.listen(8000, () =>{ // 8000 is the port number
         console.log("Server is running on port 8000");
     })
  }
