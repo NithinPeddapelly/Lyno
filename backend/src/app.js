44a19e351a252b57e86da2c54114c21387948177
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-02-21 06:13:53 +0530

    added Express router with authentication and activity-related routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,19 +13,18 @@ const server = createServer(app); // Creating an HTTP server and integrating it 
 const io = connectToSocket(server); // Initializing Socket.IO and connecting it to the server
 
 app.set("port", process.env.PORT || 8000); // Setting the port for the server (default: 8000)
-
-app.get("/home", (req, res) => {
-   return res.json({ hello: "world" }); // Responding with a JSON object containing a greeting message
-});
+app.use(cors());
+app.use(express.json({limit: "40kb"}))
+app.use(express.urlencoded({limit: "40kb", extended: true}));
 
 const start = async () => {
-   const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}`; // Constructing MongoDB connection URI
-   const connectionDb = await mongoose.connect(mongoURI); // Establishing connection to MongoDB
-   console.log(`MONGO connected DB host: ${connectionDb.connection.host}`); // Logging database connection confirmation
+  const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}`; // Constructing MongoDB connection URI
+  const connectionDb = await mongoose.connect(mongoURI); // Establishing connection to MongoDB
+  console.log(`MONGO connected DB host: ${connectionDb.connection.host}`); // Logging database connection confirmation
 
-   server.listen(app.get("port"), () => {
-      console.log(`Server is running on port ${app.get("port")}`); // Starting the server and logging confirmation
-   });
+  server.listen(app.get("port"), () => {
+    console.log(`Server is running on port ${app.get("port")}`); // Starting the server and logging confirmation
+  });
 };
 
 start(); // Initializing the server and database connection
--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -6,3 +6,4 @@ export const connectToSocket = (server)=>{
 
     return io;
 } 
+ 
