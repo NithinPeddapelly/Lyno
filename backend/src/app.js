d873d2e58bdd93788ec37736afe40248b8658fe4
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-02-21 05:34:53 +0530

    Integrated environment variables, modularized Socket.IO setup, and added MongoDB connection for improved scalability and maintainability

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
 import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
 import cors from "cors";
 
 const app = express(); // Creating an instance of Express
 const server = createServer(app); // Creating an HTTP server and integrating it with Express
-const io = new Server(server); // Setting up a new Socket.IO server instance
+const io = connectToSocket(server); // Initializing Socket.IO and connecting it to the server
 
 app.set("port", process.env.PORT || 8000); // Setting the port for the server (default: 8000)
+
 app.get("/home", (req, res) => {
-   // Defining a simple GET route at "/home"
-   return res.json({ hello: "world12" }); // Responds with a JSON object containing "hello": "world"
+   return res.json({ hello: "world" }); // Responding with a JSON object containing a greeting message
 });
 
 const start = async () => {
-  
-   // Function to start the server
+   const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}`; // Constructing MongoDB connection URI
+   const connectionDb = await mongoose.connect(mongoURI); // Establishing connection to MongoDB
+   console.log(`MONGO connected DB host: ${connectionDb.connection.host}`); // Logging database connection confirmation
+
    server.listen(app.get("port"), () => {
-      console.log("Server is running on port 8000"); // Listening on port 8000 and logging confirmation
+      console.log(`Server is running on port ${app.get("port")}`); // Starting the server and logging confirmation
    });
 };
 
-start(); // Calling the start function to initialize the server
+start(); // Initializing the server and database connection
