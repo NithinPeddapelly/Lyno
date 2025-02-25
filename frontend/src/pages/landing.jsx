ccc2405296bf4a79367047391765ecea34657514
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-02-25 23:42:41 +0530

    created and Added content to the Landing Page

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Lyno</h1>
+      <p>This is the landing page.</p>
+    </div>
+  );
+}
