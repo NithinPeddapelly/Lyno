ddf03170175ba2cffde9bb9f7ca256b0deb9a449
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-02-26 09:27:11 +0530

    Added a navbar with guest, register, and login options. Improved layout with a heading, description, and image

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <div>
-      <h1>Welcome to Lyno</h1>
-      <p>This is the landing page.</p>
+    <div className="landingPageContainer">
+        <nav>
+            <div className="navHeader">
+                <h2>LYNO</h2>
+            </div>
+
+            <div className="navlist">
+                <p>Join as a Guest</p>
+                <p>Register</p>
+                <div role="button">
+                  <p>Login</p>
+                </div>
+
+            </div>
+        </nav>
+        <div className="landingMainContainer">
+          <div>
+            <h1><span style={{color:"orange"}}>Seamless</span> Communication, Anytime, Anywhere</h1>
+          
+            <p>A fast and secure platform for online meetings and collaborations</p>
+            
+            <div role='button'>
+              <Link to={"/home"}>Get Started</Link>
+            </div>
+          </div>
+          <div>
+              <img src ="/mobile.png" alt="" />
+          </div>
+        </div>
     </div>
   );
 }
