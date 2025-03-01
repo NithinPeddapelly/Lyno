58bad5d3c103c2d317b5af127b33cd923cf776da
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-03-01 08:41:08 +0530

    Added AuthProvider context and wrapped Routes for authentication state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
+import { AuthProvider } from "./contexts/authContext";
 import Authentication from "./pages/authentication";
 import LandingPage from "./pages/landing";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    <div className = "App"> 
+
     <Router>
+
+      <AuthProvider>
+        
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<Authentication />} /> 
       </Routes>
+
+      </AuthProvider>
+      
     </Router>
+
+    </div>
+    
   );
 }
 
