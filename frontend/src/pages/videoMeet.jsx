50b078371f05ccff3c422af8f350a07e9a392ac7
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-03-02 21:09:32 +0530

    added state management, refs for video and socket handling, and a username prompt in VideoMeetComponent

--- frontend/src/pages/videoMeet.jsx
+++ frontend/src/pages/videoMeet.jsx
@@ -1,5 +1,6 @@
-import React, { use } from 'react'
+import React, { use, useState } from 'react'
 
+import "../styles/videoComponent.css";
 
 
 const serverUrl = "http://localhost:8000";
@@ -17,7 +18,39 @@ const peerConfigConnections ={
 
 export default function VideoMeetComponent() {
 
+    var socketRef = useRef();
+    let socketIdref = useRef();
+
+    let localVideoRef = useRef();
+
+    let [videoAvailable, setVideoAvailable] = useState(true);
+    let [audioAvailable, setAudioAvailable] = useState(true);
+    let [video, setVideo] = useState();
+    let [audio, setAudio] = useState();
+    let [screen, setScreen] = useState();
+    let [showModal, setModal] = useState();
+    let [screenAvailable, setScreenAvailable] = useState();
+    let [messages , setMessages] = useState([]);
+    let [message , setMessage] = useState("");
+    let [newMessages , setNewMessages] = useState(0);
+    let [askForUsername, setAskForUsername] = useState(True);
+    let [username, setUsername] = useState("");
+
+    const videoRef = useRef([]);
+
+    let[vidoes, setVideos] = useState([]);
+
+    // if(isChrome()== false){}
+
     return (
-        <div>VideoMeetComponent</div>
+        <div>
+            
+            {askForUsername == true ? 
+                <div>
+
+
+
+                </div> : <></>
+        }</div>
     )
 }
