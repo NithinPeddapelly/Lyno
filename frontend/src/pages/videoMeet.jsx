784cb1e004d96a176bdaace17761d0f0ac772eba
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-03-02 23:53:13 +0530

    added user permission handling for video, audio, and screen sharing, along with UI enhancements using Material-UI

--- frontend/src/pages/videoMeet.jsx
+++ frontend/src/pages/videoMeet.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
+import { TextField, Button, Typography, Dialog, Card } from "@mui/material";
 import "../styles/videoComponent.css";
 
 const serverUrl = "http://localhost:8000";
@@ -31,17 +32,61 @@ export default function VideoMeetComponent() {
     const [username, setUsername] = useState("");
     const [videos, setVideos] = useState([]);
 
+
+    const getPermissions = async () => {
+        try {
+            const videoPermission = await navigator.mediaDevices.getUserMedia({video: true})
+            if (videoPermission) {
+                setVideoAvailable(true);
+            } else {
+                setVideoAvailable(false);
+            }
+            const audioPermission = await navigator.mediaDevices.getUserMedia({audio: true})
+            if (audioPermission) {
+                setAudioAvailable(true);
+            } else {
+                setAudioAvailable(false);
+            }
+
+            if(navigator.mediaDevices.getDisplayMedia){
+                setScreenAvailable(true);
+            }else{
+                setScreenAvailable(false);
+            }
+
+            if(videoAvailable || audioAvailable){
+                const userMediaStream = await navigator.mediaDevices.getUserMedia({video: videoAvailable, audio: audioAvailable});
+                if(userMediaStream){
+                    window.localStream = userMediaStream;
+                    if(localVideoRef.current){
+                        localVideoRef.current.srcObject = userMediaStream;
+                    }
+                }
+            }
+        } catch{ 
+           
+        }
+    };
+
     useEffect(() => {
-     
+        getPermissions();
     }, []);
 
     return (
         <div>
-            {askForUsername === true ? 
+            {askForUsername && (
                 <div>
-                    <h2>Enter Lobby</h2>
-                </div> :<></>
-            }
+                    <h2>LOBBY</h2>
+                    <TextField id="outlined-basic" label="Username" value={username} onChange={e => setUsername(e.target.value)} variant="outlined" />
+                    <Button variant="contained">Connect</Button>
+
+
+                    <div>
+                        <video ref={localVideoRef} autoPlay muted></video>
+                    </div>
+
+                </div>
+            )}
         </div>
     );
 }
