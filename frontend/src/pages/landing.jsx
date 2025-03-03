9cb40d91b1370701b24685870b9e75ee3fd305a8
NithinPeddapelly <peddapally.nithin777@gmail.com>
2025-03-03 07:49:54 +0530

    Formated

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -5,34 +5,38 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
   return (
     <div className="landingPageContainer">
-        <nav>
-            <div className="navHeader">
-                <h2>LYNO</h2>
-            </div>
+      <nav>
+        <div className="navHeader">
+          <h2>LYNO</h2>
+        </div>
 
-            <div className="navlist">
-                <p>Join as a Guest</p>
-                <p>Register</p>
-                <div role="button">
-                  <p>Login</p>
-                </div>
-
-            </div>
-        </nav>
-        <div className="landingMainContainer">
-          <div>
-            <h1><span style={{color:"orange"}}>Seamless</span> Communication, Anytime, Anywhere</h1>
-          
-            <p>A fast and secure platform for online meetings and collaborations</p>
-            
-            <div role='button'>
-              <Link to={"/auth"}>Get Started</Link>
-            </div>
-          </div>
-          <div>
-              <img src ="/mobile.png" alt="" />
+        <div className="navlist">
+          <p>Join as a Guest</p>
+          <p>Register</p>
+          <div role="button">
+            <p>Login</p>
           </div>
         </div>
+      </nav>
+      <div className="landingMainContainer">
+        <div>
+          <h1>
+            <span style={{ color: "orange" }}>Seamless</span> Communication,
+            Anytime, Anywhere
+          </h1>
+
+          <p>
+            A fast and secure platform for online meetings and collaborations
+          </p>
+
+          <div role="button">
+            <Link to={"/auth"}>Get Started</Link>
+          </div>
+        </div>
+        <div>
+          <img src="/mobile.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
