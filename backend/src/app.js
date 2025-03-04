8ba4dc9d1d0016226dede5a9b38d702cfc506372
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-04 06:57:38 -0800

    Removed duplicate code and ensured a clean server setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,17 +16,18 @@ const io = connectToSocket(server); // Initializing Socket.IO and connecting it 
 
 app.set("port", process.env.PORT || 8000); // Setting the port for the server (default: 8000)
 app.use(cors());
-app.use(express.json({limit: "40kb"}))
-app.use(express.urlencoded({limit: "40kb", extended: true}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.use("/api/v1/users",userRoutes); // Using the user routes for all endpoints starting with /api/v1"old users"/users - just in case they didnt update to new api
+app.use("/api/v1/users", userRoutes); // Using the user routes for all endpoints starting with /api/v1"old users"/users - just in case they didnt update to new api
 
 const start = async () => {
   const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}`; // Constructing MongoDB connection URI
   const connectionDb = await mongoose.connect(mongoURI); // Establishing connection to MongoDB
   console.log(`MONGO connected DB host: ${connectionDb.connection.host}`); // Logging database connection confirmation
 
-  server.listen(app.get("port"), () => { // Starting the server
+  server.listen(app.get("port"), () => {
+    // Starting the server
     console.log(`Server is running on port ${app.get("port")}`); // Starting the server and logging confirmation
   });
 };
@@ -36,6 +37,5 @@ const start = async () => {
 app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is connected!" });
 });
-
 
 start(); // Initializing the server and database connection
