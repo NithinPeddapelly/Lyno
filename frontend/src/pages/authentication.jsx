bf0fbe30d9c90dd004d92c156fc7935480932e8a
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-13 19:15:30 +0530

    Auth bug

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -12,7 +12,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { AuthContext } from "../contexts/authContext";
+import { AuthContext } from "../contexts/AuthContext";
 import Snackbar from "@mui/material/Snackbar";
 import { red } from "@mui/material/colors";
 
