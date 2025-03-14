377b1890f111be9df49cd21a88c60ac195470996
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-15 04:47:54 +0530

    Fixed import path casing for AuthContex and Added History page route and import for navigation support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider } from "./contexts/authContext";
 import Authentication from "./pages/authentication";
 import LandingPage from "./pages/landing";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VideoMeetComponent from "./pages/videoMeet";
 import HomeComponent from "./pages/home";
+import History from "./pages/history";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path="/auth" element={<Authentication />} />
             <Route path="/:url" element={<VideoMeetComponent />} />
             <Route path="/home" element={<HomeComponent />} />
+            <Route path="/history" element={<History/>} />
           </Routes>
         </AuthProvider>
       </Router>
