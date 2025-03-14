347b8f1de68d34cc0c10573170d9f0a170fea3f3
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-14 21:42:03 +0530

    added History component

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { User } from "../models/user.model.js";
 import httpStatus from "http-status";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
+import { Meeting } from "../models/meeting.model.js";
 
 // Handles user login by verifying credentials and generating an authentication token.
 const login = async (req, res) => {
@@ -64,5 +65,35 @@ const register = async (req, res) => {
         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "An error occurred. Please try again." });
     }
 };
+const getUserHistory = async (req, res) => {
+    const { token } = req.query;
 
-export { login, register };
+    try {
+        const user = await User.findOne({ token: token });
+        const meetings = await Meeting.find({ user_id: user.username })
+        res.json(meetings)
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+}
+
+const addToHistory = async (req, res) => {
+    const { token, meeting_code } = req.body;
+
+    try {
+        const user = await User.findOne({ token: token });
+
+        const newMeeting = new Meeting({
+            user_id: user.username,
+            meetingCode: meeting_code
+        })
+
+        await newMeeting.save();
+
+        res.status(httpStatus.CREATED).json({ message: "Added code to history" })
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+}
+
+export { login, register,getUserHistory,addToHistory };
