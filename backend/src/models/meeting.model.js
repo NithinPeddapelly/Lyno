e1b977d4031f076ae9330342cf447d254e2418e7
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-16 19:12:01 +0530

    Updated

--- backend/src/models/meeting.model.js
+++ backend/src/models/meeting.model.js
@@ -1,13 +1,14 @@
-import mongoose, { Schema } from "mongoose"; // Importing mongoose and Schema
+import mongoose, { Schema } from "mongoose";
 
-const meetingSchema = new Schema( 
+
+const meetingSchema = new Schema(
     {
-        user_id: { type: String }, 
+        user_id: { type: String },
         meetingCode: { type: String, required: true },
-        date: { type: Date, default: Date.now, required: true } // Defining the meeting schema to store user's meeting details
+        date: { type: Date, default: Date.now, required: true }
     }
-);
+)
 
-const Meeting = mongoose.model("Meeting", meetingSchema); // Creating a user model from the schema
+const Meeting = mongoose.model("Meeting", meetingSchema);
 
-export { Meeting }; // Exporting
+export { Meeting };
