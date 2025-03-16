4769826bc2cbefa216cd8311a8c94fcca3bcd060
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-16 20:33:59 +0530

    corrections after removing Vlite

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { ThemeProvider } from '@mui/material/styles';
-import AppTheme from './shared-theme/AppTheme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@mui/material/styles";
+import AppTheme from "./shared-theme/AppTheme";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <ThemeProvider theme={AppTheme}>
     <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+  </ThemeProvider>
 );
