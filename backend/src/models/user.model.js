d9b94355e4c979f55238c610565a5b71484c899f
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-20 18:19:45 +0530

    Formated

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,13 +1,12 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema( // Defining the user schema to store user details
-    {
-        name: { type: String, required: true }, // User's full name (required)
-        username: { type: String, required: true, unique: true }, // Unique username for login
-        password: { type: String, required: true }, // Hashed password for authentication
-        token: { type: String } // JWT token for session management (optional)
-    }
-);
-const User = mongoose.model("User", userSchema); // Creating a User model from the schema
+const userScheme = new Schema({ // Defining the user schema to store user details
+  name: { type: String, required: true }, // User's full name (required)
+  username: { type: String, required: true, unique: true }, // Unique username for login
+  password: { type: String, required: true }, // Hashed password for authentication
+  token: { type: String }, // JWT token for session management
+});
 
-export { User }; // Exporting
+const User = mongoose.model("User", userScheme); // Creating a User model from the schema
+
+export { User };
