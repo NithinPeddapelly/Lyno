64df530d44b55587c4e4819b5a0a3290be54f961
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-20 19:55:50 +0530

    Deployment trail 1

---
+++ frontend/src/environment.js
@@ -0,0 +1,4 @@
+let IS_PROD = true;
+const server = IS_PROD ? "http://localhost:3000" : "https://lyno.onrender.com"; //conditional rendering
+
+export default server;
--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -11,8 +11,9 @@ import MicOffIcon from '@mui/icons-material/MicOff';
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
 import ChatIcon from '@mui/icons-material/Chat';
+import server from "../environment";
 
-const serverUrl = "http://localhost:8000";
+const serverUrl = server.prod; // Server URL
 
 var connections = {}; // Object to hold Peer connections for each user
 
