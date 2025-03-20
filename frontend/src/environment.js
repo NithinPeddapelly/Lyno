76404e0e45130c4cbb1217f5ab47c05247e37225
Nithin Peddapelly <peddapally.nithin777@gmail.com>
2025-03-20 22:16:55 +0530

    Deployment trail 3 - port correction

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
 let IS_PROD = true;
-const server = IS_PROD ? "http://localhost:3000" : "https://lyno.onrender.com"; //conditional rendering
+const server = IS_PROD ? "http://localhost:8000" : "https://lyno.onrender.com"; //conditional rendering
 
 export default server;
