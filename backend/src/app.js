845ade5da844389cad01d537a2ef7e163fbbb8c4
Nithin Peddapelly <nithinpeddapelly@gmail.com>
2026-05-21 23:22:57 +0100

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -56,7 +56,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
